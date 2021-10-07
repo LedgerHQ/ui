0ac89fe2cf3c8c8bbd9ea08beb769f234ce62d05
Thomas Laforge <thomas.laforge@ledger.fr>
2021-10-07 16:42:30 +0200

    badge story

---
+++ packages/react/src/components/toasts/Badge/Badge.stories.tsx
@@ -0,0 +1,35 @@
+import React, { useState } from "react";
+import BadgeComponent, { Props } from "@components/toasts/Badge";
+import Text from "@components/asorted/Text";
+
+export default {
+  title: "Toasts/Badge",
+  component: BadgeComponent,
+  argTypes: {
+    value: {
+      control: { type: "number" },
+    },
+  },
+};
+
+const badges = ["Tomate", 2];
+
+function Sample(args: Props) {
+  return (
+    <div style={{ marginBottom: "10px" }}>
+      <BadgeComponent {...args} />
+    </div>
+  );
+}
+
+export const Badge = (args: Props): React.ReactNode[] =>
+  [
+    <Sample value={args.value} />,
+    ...badges.reduce<React.ReactNode[]>((acc, _, index) => {
+      return [...acc, <Sample value={_} key={index} />];
+    }, [])
+  ];
+
+Badge.args = {
+  value: 4,
+};
