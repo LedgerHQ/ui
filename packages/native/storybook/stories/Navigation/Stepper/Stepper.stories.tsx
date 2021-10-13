fa36172f7103d60fa9212d4a94e33c406b865c78
Gabriel R. Soares <gabriel.soares@ledger.fr>
2021-10-13 17:22:04 +0200

    Fixed storybook props for stepper

--- packages/native/storybook/stories/Navigation/Stepper/Stepper.stories.tsx
+++ packages/native/storybook/stories/Navigation/Stepper/Stepper.stories.tsx
@@ -1,5 +1,5 @@
 import { storiesOf } from "@storybook/react-native";
-import { withKnobs, number, object, boolean } from "@storybook/addon-knobs";
+import { withKnobs, number, array, boolean } from "@storybook/addon-knobs";
 import React from "react";
 import Stepper from "@components/Navigation/Stepper";
 import CenterView from "../../CenterView";
@@ -9,11 +9,11 @@ storiesOf("Navigation", module)
   .addDecorator((getStory) => <CenterView>{getStory()}</CenterView>)
   .add("Stepper", () => (
     <Stepper
-      steps={object("steps", [
-        { label: "First step" },
-        { label: "Second step" },
-        { label: "Disabled step", disabled: true },
-        { label: "Fourth step" },
+      steps={array("steps", [
+        "First step",
+        "Second step",
+        "Third step",
+        "Fourth step",
       ])}
       activeIndex={number("activeIndex", 0, { min: 0, max: 6 })}
       errored={boolean("errored", false)}
