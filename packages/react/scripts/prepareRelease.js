cd81799bf5cbe45d5a0064a64e1fad9499ab8e7e
jelbaz-ledger <julien.el-baz@ledger.fr>
2021-10-13 10:18:14 +0200

    core(react): refine release & build script

---
+++ packages/react/scripts/prepareRelease.js
@@ -0,0 +1,33 @@
+const path = require("path");
+const fs = require("fs-extra");
+const child_process = require("child_process");
+
+const destination = path.join(__dirname, "..", "lib");
+
+// Cleanup destination folder.
+fs.removeSync(destination);
+
+// Build step.
+child_process.execSync("npm run build", { stdio: "inherit" });
+
+// Copy files that we want to include in the published package.
+const requiredFiles = [
+  "package.json",
+  "README.md",
+  "LICENSE",
+  "src/assets/images",
+  "src/assets/fonts",
+];
+requiredFiles.forEach((filename) => {
+  const suffix = filename.startsWith("src/") ? filename.slice(4) : filename;
+  const filePath = path.join(__dirname, "..", filename);
+  const destPath = path.join(destination, suffix);
+  fs.copySync(filePath, destPath);
+});
+
+// Remove the private and script fields from the package json file.
+const packageJsonPath = path.join(destination, "package.json");
+const package = require(packageJsonPath);
+delete package.private;
+delete package.scripts;
+fs.writeFileSync(packageJsonPath, JSON.stringify(package, null, 2));
