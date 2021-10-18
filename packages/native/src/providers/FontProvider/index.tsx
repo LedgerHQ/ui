81eacfd8e0fda5dc919ce8b8f345c4003bf92db7
jelbaz-ledger <julien.el-baz@ledger.fr>
2021-10-18 11:07:51 +0200

    ci: use up to date web storybook

--- packages/native/src/providers/FontProvider/index.tsx
+++ packages/native/src/providers/FontProvider/index.tsx
@@ -1,13 +1,19 @@
 import React from "react";
 import { useFonts } from "expo-font";
 
-export default ({ children }: { children: React.ReactNode }) => {
+export default ({
+  waitUntilLoaded,
+  children,
+}: {
+  waitUntilLoaded?: boolean;
+  children: React.ReactNode;
+}) => {
   const [fontsLoaded] = useFonts({
     Inter: require("../../assets/fonts/inter/Inter-Regular.otf"),
     Alpha: require("../../assets/fonts/alpha/HMAlphaMono-Medium.otf"),
   });
 
-  if (!fontsLoaded) {
+  if (waitUntilLoaded && !fontsLoaded) {
     return null;
   }
 
