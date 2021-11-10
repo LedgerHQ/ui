a206ee079278e29dc7b44f828358ca6315b73105
nparigi-ledger <nicolas.parigi@ledger.fr>
2021-11-10 12:19:53 +0100

    Type fix

--- packages/react/src/components/layout/Box/index.ts
+++ packages/react/src/components/layout/Box/index.ts
@@ -1,9 +1,10 @@
 import styled from "styled-components";
 import { sharedStyle, SharedStyleProps } from "../../../styles/system/shared";
-const Box = styled.div<SharedStyleProps>`
-  ${sharedStyle};
-`;
 
 export type BoxProps = React.HTMLAttributes<HTMLDivElement> & SharedStyleProps;
+
+const Box = styled.div<BoxProps>`
+  ${sharedStyle};
+`;
 
 export default Box;
