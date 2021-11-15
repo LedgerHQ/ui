b7008dc1531fc2a1d65c1878f26ea4f7ac85507d
LFBarreto <lfb.access@gmail.com>
2021-11-15 17:04:44 +0100

    (react): base styled cleanup

--- packages/react/src/components/styled.ts
+++ packages/react/src/components/styled.ts
@@ -22,39 +22,6 @@ import {
   OverflowProps,
 } from "styled-system";
 
-type styledFnArgs = [
-  first: CSSObject | InterpolationFunction<any> | TemplateStringsArray,
-  ...rest: Interpolation<any>[]
-];
-
-function styledFunc(tag: AnyStyledComponent) {
-  const func = (...args: any) =>
-    styled(tag).apply(styled, <styledFnArgs>(
-      (<unknown>[...args, compose(flexbox, space, position, color, layout, overflow, gaps)])
-    ));
-
-  func.attrs =
-    (attrs: any) =>
-    (...args: any) =>
-      styled(tag)
-        .attrs(attrs)
-        .apply(styled, <styledFnArgs>(
-          (<unknown>[...args, compose(flexbox, space, position, color, layout, overflow, gaps)])
-        ));
-
-  return func;
-}
-
-Object.keys(styled).forEach((htmlTag: string) => {
-  Object.defineProperty(styledFunc, htmlTag, {
-    value: styledFunc(<AnyStyledComponent>htmlTag),
-  });
-});
-
-const wrappedStyled: unknown = styledFunc;
-
-export default <StyledInterface>wrappedStyled;
-
 export const baseStyles = compose(flexbox, space, position, color, layout, overflow, gaps);
 
 export type BaseStyledProps = SpaceProps &
@@ -68,3 +35,32 @@ export type BaseStyledProps = SpaceProps &
     color?: string;
     display?: string;
   };
+
+type styledFnArgs = [
+  first: CSSObject | InterpolationFunction<any> | TemplateStringsArray,
+  ...rest: Interpolation<any>[]
+];
+
+function styledFunc(tag: AnyStyledComponent) {
+  const func = (...args: any) =>
+    styled(tag).apply(styled, <styledFnArgs>(<unknown>[...args, baseStyles]));
+
+  func.attrs =
+    (attrs: any) =>
+    (...args: any) =>
+      styled(tag)
+        .attrs(attrs)
+        .apply(styled, <styledFnArgs>(<unknown>[...args, baseStyles]));
+
+  return func;
+}
+
+Object.keys(styled).forEach((htmlTag: string) => {
+  Object.defineProperty(styledFunc, htmlTag, {
+    value: styledFunc(<AnyStyledComponent>htmlTag),
+  });
+});
+
+const wrappedStyled: unknown = styledFunc;
+
+export default <StyledInterface>wrappedStyled;
