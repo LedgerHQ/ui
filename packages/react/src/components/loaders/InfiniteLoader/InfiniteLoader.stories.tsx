4d076ddc98c67e1594bf9edc32329a1aec70fe15
elbywan <julien.el-baz@ledger.fr>
2021-12-09 14:12:14 +0100

    chore(react): use handcrafted svg for the infinite loader

--- packages/react/src/components/loaders/InfiniteLoader/InfiniteLoader.stories.tsx
+++ packages/react/src/components/loaders/InfiniteLoader/InfiniteLoader.stories.tsx
@@ -8,9 +8,24 @@ export default {
     size: {
       type: "number",
     },
+    color: {
+      control: {
+        type: "color",
+        presetColors: ["coral", "tomato", "orange", "blue", "purple"],
+      },
+    },
+    theme: {
+      table: { disable: true },
+    },
+    as: {
+      table: { disable: true },
+    },
+    forwardedAs: {
+      table: { disable: true },
+    },
   },
 };
 
 export const Default = (args: Props): JSX.Element => {
-  return <InfiniteLoader size={args.size} />;
+  return <InfiniteLoader {...args} />;
 };
